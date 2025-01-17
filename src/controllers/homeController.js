e2fcd1d4059b41d0071dfc937f65b5c6e13c1573
DuyTran <duytran@DuyTrans-MacBook-Pro.local>
2025-01-17 17:22:32 -0500

    optimize code in homeController, and design List users static

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,45 @@
 const connection = require('../config/dataBase')
 
-const getHomePage = (req, res) => {
-    return res.render('home')
+const getHomePage = async (req, res) => {
+    const [results, fields] = await connection.query(
+        `SELECT * FROM Users`
+    )
+    console.log('results = ', results)
+    return res.render('home', results)
 }
 
-const postCreateUser = (req, res) => {
-    console.log(req.body)
+const postCreateUser = async (req, res) => {
+    let email = req.body.email
+    let name = req.body.name
+    let city = req.body.city
+
+    // connection.query(
+    //     `INSERT INTO 
+    //         Users(email, name, city) 
+    //     VALUES(?, ?, ?)`,
+    //     [email, name, city],
+    //     (err, results, fields) => {
+    //         res.send('create user successfully')
+    //         console.log("results: ", results)
+    //     }
+    // )
+
+    let [results, fields] = await connection.query(
+        `INSERT INTO 
+            Users(email, name, city)
+        VALUES (?,?,?)`,
+        [email, name, city]
+    )
+    console.log('results = ', results)
+    res.send('create user successfully')
+}
+
+const getCreateUser = (req, res) => {
+    return res.render('createUser')
 }
 
 module.exports = {
     getHomePage,
-    postCreateUser
+    postCreateUser,
+    getCreateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getHomePage, postCreateUser } = require('../controllers/homeController')
+const { getHomePage, postCreateUser, getCreateUser } = require('../controllers/homeController')
 
 router.get('/home', getHomePage)
 router.post('/create-user', postCreateUser)
+router.get('/create-user', getCreateUser)
 
 module.exports = router
