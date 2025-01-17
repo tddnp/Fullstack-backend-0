30aefd3c298f05122f5cf3a42857093c27dd357d
DuyTran <duytran@DuyTrans-MacBook-Pro.local>
2025-01-16 19:34:17 -0500

    connect to database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRouter = require('./routes/web')
+const mysql = require('mysql2')
 
 const app = express() // app express
 const port = process.env.PORT || 3000 // port
@@ -12,6 +13,23 @@ configViewEngine(app);
 
 //initiate routes
 app.use('/', webRouter)
+
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+})
+
+connection.query(
+    'SELECT * FROM Users',
+    (err, results, fields) => {
+        console.log('results = ',results)
+        console.log('fields = ',fields)
+    }
+)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
