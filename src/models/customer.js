b32cb1a22878d6b75bb6873d68176ccc0974433d
DuyTran <duytran@DuyTrans-MacBook-Pro.local>
2025-01-28 05:14:28 -0500

    adding customer model

---
+++ src/models/customer.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+// data shape
+const customerSchema = new mongoose.Schema(
+    {
+        name: { type: String, require: true },
+        address: String,
+        phone: String,
+        email: String,
+        image: String,
+        description: String
+    },
+    {
+        timestamps: true
+    }
+);
+const Customer = mongoose.model('Users', customerSchema);
+
+module.exports = Customer
